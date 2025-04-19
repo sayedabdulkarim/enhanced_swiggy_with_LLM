9c958a5fc5086b52e0ae08066ea8f71e175f4900
Sayed Abdul <sakarim9124@gmail.com>
2025-04-19 23:35:57 +0530

    refactor: clean up CORS middleware setup and improve preflight request handling

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ import cors from "cors";
 dotenv.config();
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDb from "./config/db.js";
-
 //routes
 import userRoutes from "./routes/userRoutes.js";
 import homeRoutes from "./routes/homeRoutes.js";
@@ -27,28 +26,26 @@ connectDb();
 
 const app = express();
 
-// CORS middleware setup (before routes)
+//
+// Increase the limit for parsed data (JSON)
+app.use(express.json({ limit: "50mb" })); // Adjust '50mb' as needed
+// Increase the limit for parsed data (URL-encoded)
+app.use(express.urlencoded({ limit: "50mb", extended: true })); // Adjust '50mb' as needed
+
 const corsOptions = {
   origin: [
     "http://localhost:3000",
     "http://localhost:3001",
-    "https://fooddelivery-core-client.vercel.app",
+    "https://food-delivery-ab.vercel.app",
+    "https://food-delivery-admin-one.vercel.app",
   ], // Client's URL, not the server's
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, // <-- REQUIRED backend setting
 };
 
-app.use(cors(corsOptions)); // This should be before routes
+app.use(cors(corsOptions));
 
-// Handling OPTIONS request for preflight requests
-app.options("*", cors(corsOptions)); // This will handle all preflight requests
-
-// Increase the limit for parsed data (JSON)
-app.use(express.json({ limit: "50mb" }));
-// Increase the limit for parsed data (URL-encoded)
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-// Cookie parser
+// app.use(cors());
 app.use(cookieParser());
 
 //test
